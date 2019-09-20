a6f0678836c6567a03bad61a00d1888bdcc2fe73
tony <598291606@qq.com>
2019-09-20 22:08:44 +0800

    进入游戏 和 离开游戏
    进入游戏 和 离开游戏

--- game-server/app/servers/mary_slot/base/table.ts
+++ game-server/app/servers/mary_slot/base/table.ts
@@ -1,5 +1,7 @@
 import { Application } from "pinus";
 import { make_slot_reward } from "./make";
+import { getConnection, Connection } from "typeorm";
+import { User_MOG } from "../../../entity/User_MOG";
 
 let xmcommon = require('xmcommon');
 let utils = xmcommon.utils;
@@ -55,6 +57,8 @@ export class Mary_Slot_Table {
     private null_reward_num: number;
     private small_game_num: number;
     private free_game_num: number;
+    private xue_game: Connection; // 用户数据库连接
+    private user: User_MOG;
 
     public static createTable(app: Application,room_index:number) {
         return new Mary_Slot_Table(app,room_index);
@@ -66,7 +70,7 @@ export class Mary_Slot_Table {
         this.room_config = app.get('HappyFruit_'+room_index);
         this.table_id = Mary_Slot_Table.TABLE_ID;
         Mary_Slot_Table.TABLE_ID ++;
-        Mary_Slot_Table.ROOM_LIST[this.table_id] = this;
+        Mary_Slot_Table.ROOM_LIST.set(this.table_id,this);
         this.null_reward_num = 0; // 连续空奖次数
         this.small_game_num = 0; // 小游戏剩余次数
         this.free_game_num = 0; // 免费转次数
@@ -80,17 +84,62 @@ export class Mary_Slot_Table {
         let REDIS_HSETNX = global['REDIS_HSETNX'];
         let Init = this.room_config.RoomControl.Init;
         await utils.WaitFunctionEx(REDIS_HSETNX, 'Mary_Slot', 'room_pool', Init);
+        const xue_game = getConnection('xue_game');
+        this.xue_game = xue_game;
     }
 
     /**
      *  下注摇奖
      */
     async put_bet(bet:number) {
+        if (this.user == null) return {code:402,data:"用户未进入房间."};
+        let is_free:boolean = false;
+        let one_bet:number = bet / 9;
+        if (this.room_config.Bet.indexOf(one_bet) == -1) return {code:403,data:"压注格式错误."}
+        if (this.free_game_num > 0) {
+            this.free_game_num --;
+            is_free = true;
+            one_bet = this.room_config.Bet[0];
+        }else{
+            is_free = false;
+            this.user.coin -= bet;
+        }
         let REDIS_HGET = global['REDIS_HGET'];
         let room_pool = await utils.WaitFunctionEx(REDIS_HGET, 'Mary_Slot', 'room_pool');
-        if(room_pool[0] != null) return {code:403};
+        if(room_pool[0] != null) return {code:404,data:"奖池读取错误."};
         room_pool = ~~room_pool[1] || 0;
         let reward = make_slot_reward(room_pool,this.room_config);
+        ///_____ 结算中奖金额TODO:
+        
+        await this.xue_game.manager.save(this.user);
         return reward;
+    }
+
+    /**
+     * 玩家进入游戏
+     * @param uid 
+     */
+    async enter_game(uid:number) {
+        let user = await this.xue_game.manager.findOne(User_MOG,{uid});
+        if (user == null) {
+            return {code:403,data:'玩家不存在.'};
+        }
+        this.user = user;
+        return {code:0};
+    }
+
+    
+    /**
+     * 玩家离开游戏
+     * @param uid 
+     */
+    async leave_game(uid:number) {
+        if(this.user && this.user.uid == uid) {
+            this.user = null;
+            Mary_Slot_Table.ROOM_LIST.delete(this.table_id);
+            return {code:0};
+        }else{
+            return {code:404,data:"你不在该房间中"};
+        }
     }
 }
